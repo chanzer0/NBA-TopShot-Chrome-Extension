9d70b5103d178003bf789ca3a21963d4bc1b1ed3
Sean <seansailer28@gmail.com>
2021-02-01 12:23:27 -0600

    fixed a few bugs

--- priceMonitor.js
+++ priceMonitor.js
@@ -24,10 +24,13 @@ function addPriceMonitor() {
                 return false;
             });
             var guid = window.location.href.split("/")[5];
+            if (guid.indexOf("?") > -1 ){
+                guid = guid.split("?")[0];
+            }
             var monString = "monitoringEnabled" + guid;
             chrome.storage.sync.get(monString, (obj) => {
                 if (Object.keys(obj).length === 0 && obj.constructor === Object) {
-                    chrome.storage.sync.set({ "monitoringEnabled": false });
+                    chrome.storage.sync.set({ [monString]: false });
                     return;
                 } else {
                     if (obj[monString]) {
@@ -51,11 +54,11 @@ function addPriceMonitor() {
                                 if (parseInt(minPrice.replace(/[,\$]/g, '')) < data[priceString]) {
                                     // Check if serial condition has been met (if applicable)
                                     if (data[serialString] != null &&  data[serialString].length != 0) {
-                                        var dropdown = $('#moment-detailed-serialNumber').get();
-                                        var optionsList = dropdown.options;
-                                        optionsList = optionsList.filter(el => el.value < data[serialString]);
-                                        if (optionsList.some(el => el.price < data[priceString])) {
-                                            alertUser();
+                                        var dropdown = document.getElementById('moment-detailed-serialNumber');
+                                        var optionsList = Array.from(dropdown.options);
+                                        var newArr = optionsList.filter(el => el.value < +data[serialString]);
+                                        if (newArr.length > 0) {
+                                            $('#moment-detailed-serialNumber').val(newArr[0].value);
                                         }
                                     } else {
                                         alertUser();
@@ -72,7 +75,6 @@ function addPriceMonitor() {
 }
 
 function alertUser() {
-    window.clearTimeout();
     resetMonitoring();
     var buyButton = $('button[data-testid="button-p2p-purchase-moment"]');
     buyButton.click();
@@ -80,7 +82,11 @@ function alertUser() {
 }
 
 function resetMonitoring() {
+    window.clearTimeout();
     var guid = window.location.href.split("/")[5];
+    if (guid.indexOf("?") > -1 ){
+        guid = guid.split("?")[0];
+    }
 
     var monString = "monitoringEnabled" + guid;
     var priceString = "priceToAlert" + guid;
@@ -91,7 +97,6 @@ function resetMonitoring() {
     chrome.storage.sync.set({ [priceString]: null });
     chrome.storage.sync.set({ [serialString]: null });
     chrome.storage.sync.set({ [refreshString]: null });
-    chrome.storage.sync.set({ [refreshString]: null });
     $("#disableMonitor").toggle();
 }
 
@@ -101,6 +106,9 @@ function beginMonitoring() {
     var refreshInterval = $("#alertForm #refreshInput").val().trim();
 
     var guid = window.location.href.split("/")[5];
+    if (guid.indexOf("?") > -1 ){
+        guid = guid.split("?")[0];
+    }
 
     var monString = "monitoringEnabled" + guid;
     var priceString = "priceToAlert" + guid;
